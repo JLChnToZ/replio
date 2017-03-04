dc0be69a0a316c9ff69f02c743d1562686d1cb46
JLChnToZ <jeremylam619@gmail.com>
2017-03-04 16:00:51 +0800

    Add multi-window

--- index.js
+++ index.js
@@ -4,47 +4,73 @@ const url = require('url');
 const Repl = require('repl');
 const MemoryStream = require('memorystream');
 
-let win, repl;
+const instances = {};
 
-const input = new MemoryStream(), output = new MemoryStream();
-// Tell the repl that these streams are raw TTY as hterm is handling these.
-input.isTTY = input.isRaw = output.isTTY = output.isRaw = true;
+function getTTYStream() {
+  const stream = new MemoryStream();
+  stream.isTTY = stream.isRaw = true;
+  return stream;
+}
 
-app.on('ready', () => {
-  win = new BrowserWindow({ width: 800, height: 600 });
-  
-  win.setMenu(null);
-  
-  ipcMain.on('terminal-ready', initIPC);
-
-  win.loadURL(url.format({
+function createWindow() {
+  const instance = {
+    input: getTTYStream(),
+    output: getTTYStream(),
+    repl: null,
+    win: new BrowserWindow({ width: 800, height: 600 })
+  };
+  const id = instance.win.webContents.id;
+  instances[id] = instance;
+  instance.output.on('readable', () => {
+    if(!instance.win) return;
+    instance.win.webContents.send('output',
+      instance.output.read()
+      .toString('utf8')
+      .replace(/\r\n|\r|\n/g, '\r\n')
+    );
+  });
+  instance.win.setMenu(null);
+  instance.win.loadURL(url.format({
     pathname: path.join(__dirname, './static/index.html'),
     protocol: 'file:',
     slashes: true
   }));
+  instance.win.on('closed', () => {
+    instance.win = null;
+    if(instance.repl)
+      instance.repl.close();
+    delete instances[id];
+  });
+}
 
-  win.on('closed', () => {
-    win = null;
-    if(repl) repl.close();
+ipcMain.on('terminal-ready', (e) => {
+  const instance = instances[e.sender.id];
+  if(!instance) return;
+  instance.repl = Repl.start({
+    input: instance.input,
+    output: instance.output
+  }).on('close', () => {
+    instance.repl = null;
+    if(instance.win)
+      instance.win.close();
+  });
+  instance.repl.defineCommand('spawn', {
+    help: 'Spawn a new REPL instance',
+    action() {
+      createWindow();
+      instance.output.write('Spawning new instance...\n', 'utf8');
+      this.displayPrompt();
+    }
   });
 });
 
-function initIPC() {
-  ipcMain.on('input', (e, a) => {
-    input.write(Buffer.from(a, 'utf8'));
-  });
+ipcMain.on('input', (e, data) => {
+  const instance = instances[e.sender.id];
+  if(!instance) return;
+  instance.input.write(Buffer.from(data, 'utf8'));
+});
 
-  output.on('readable', () => {
-    win.webContents.send('output', output.read().toString('utf8').replace(/\r\n|\r|\n/g, '\r\n'));
-  });
-  
-  repl = Repl
-  .start({ input, output })
-  .on('close', () => {
-    repl = null;
-    if(win) win.close();
-  });
-}
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if(process.platform !== 'darwin')
@@ -52,6 +78,7 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
-  if(win === null)
-    createWindow();
+  for(let id in instances)
+    return;
+  createWindow();
 });
