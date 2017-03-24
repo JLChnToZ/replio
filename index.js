44b1e17842003896eeb64eddfecc452a86a18c22
JLChnToZ <jeremylam619@gmail.com>
2017-03-24 17:47:31 +0800

    Split the large thing into small modules

--- index.js
+++ index.js
@@ -1,126 +1,51 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const net = require('net');
-const path = require('path');
-const url = require('url');
-const Repl = require('repl');
-const MemoryStream = require('memorystream');
+const { app } = require('electron');
+const TerminalHost = require('./lib/termhost');
+const Shell = require('./lib/shellbase');
+const ReplShell = require('./lib/shells/repl');
+const TelnetShell = require('./lib/shells/telnet');
 
-const instances = {};
-
-function createWindow() {
-  const instance = {
-    input: new MemoryStream(),
-    output: new MemoryStream(),
-    repl: null,
-    win: new BrowserWindow({ width: 800, height: 600 })
-  };
-  instance.input.isRaw = true;
-  instance.output.isTTY = true;
-  const id = instance.win.webContents.id;
-  instances[id] = instance;
-  instance.output.on('readable', () => {
-    if(!instance.win) return;
-    instance.win.webContents.send('output',
-      instance.output.read()
-      .toString('utf8')
-      .replace(/\r\n|\r|\n/g, '\r\n')
-    );
-  });
-  instance.win.setMenu(null);
-  instance.win.loadURL(url.format({
-    pathname: path.resolve(__dirname, './static/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
-  instance.win.on('closed', () => {
-    instance.win = null;
-    if(instance.repl)
-      instance.repl.close();
-    delete instances[id];
-  });
+function createWindow(ShellType) {
+  if(!ShellType) ShellType = ReplShell;
+  const shell = new ShellType();
+  const host = new TerminalHost(shell);
+  if(ShellType === ReplShell)
+    registerHelperToRepl(shell);
+  return host;
 }
 
-function connectTelnet(host, port, instance) {
-  const telnet = net.connect(port, host);
-  const input = instance.input;
-  telnet.on('connect', () => {
-    instance.input = telnet;
-    telnet.pipe(instance.output, { end: false });
-  });
-  telnet.on('error', (err) => {
-    instance.output.write(`\nError: ${err.stack || err}\n\n`, 'utf8');
-    instance.input = input;
-    delete instance.onClose;
-    this.displayPrompt();
-  });
-  telnet.on('close', () => {
-    instance.input = input;
-    delete instance.onClose;
-    this.displayPrompt();
-  });
-  instance.onClose = () => {
-    telnet.close();
-    delete instance.onClose;
-  };
-}
-
-ipcMain.on('terminal-ready', (e) => {
-  const instance = instances[e.sender.id];
-  if(!instance) return;
-  instance.output.write('\x1b[36;01mReplio Standalone\x1b[0m - An experimental REPL shell for Node.js/Electron\n', 'utf8');
-  instance.output.write('Type `.help` for list of available commands\n\n', 'utf8');
-  instance.repl = Repl.start(instance).on('close', () => {
-    instance.repl = null;
-    if(instance.win)
-      instance.win.close();
-    if(instance.onClose)
-      instance.onClose();
-  });
-  instance.repl.defineCommand('spawn', {
+function registerHelperToRepl(shell) {
+  const console = shell.console;
+  shell.repl.defineCommand('spawn', {
     help: 'Spawn a new REPL instance',
     action() {
-      instance.output.write('Spawning new instance...\n\n', 'utf8');
-      createWindow();
+      console.log('Spawning new REPL instance...');
+      createWindow(ReplShell);
       this.displayPrompt();
     }
   });
-  instance.repl.defineCommand('devtools', {
-    help: 'Open developer tools for current window',
-    action(mode) {
-      instance.output.write('Opening developer tools...\n\n', 'utf8');
-      instance.win.webContents.openDevTools({ mode });
+  shell.repl.defineCommand('testing', {
+    help: 'Open an empty echo shell window for testing',
+    action() {
+      console.log('Opening new window with devTools...');
+      const host = createWindow(Shell);
+      host.window.webContents.openDevTools();
       this.displayPrompt();
     }
   });
-  instance.repl.defineCommand('.', {
+  shell.repl.defineCommand('.', {
     help: '',
     action(secret) {
       switch(secret) {
-        case 'starwars': {
-          instance.output.write('Starting star wars...\n', 'utf8');
-          connectTelnet.call(this, 'towel.blinkenlights.nl', 23, instance);
-          return;
-        }
+        case 'starwars': // Nothing but easter eggs
+          new TerminalHost(new TelnetShell('towel.blinkenlights.nl'));
+        break;
       }
       this.displayPrompt();
     }
   });
-});
+}
 
-ipcMain.on('input', (e, data) => {
-  const instance = instances[e.sender.id];
-  if(!instance) return;
-  instance.input.write(Buffer.from(data, 'utf8'));
-});
-
-ipcMain.on('resize', (e, columns, rows) => {
-  const instance = instances[e.sender.id];
-  if(!instance) return;
-  Object.assign(instance.output, { columns, rows });
-  instance.output.emit('resize');
-});
-
-app.on('ready', createWindow);
+app.on('ready', createWindow.bind(this, null));
 
 app.on('window-all-closed', () => {
   if(process.platform !== 'darwin')
@@ -128,7 +53,6 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
-  for(let id in instances)
-    return;
-  createWindow();
+  if(ShellHost.count() <= 0)
+    createWindow();
 });
